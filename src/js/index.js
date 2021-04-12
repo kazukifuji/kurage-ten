b74e9839e7e943a03c8c9ecb85ff54363a4d13a3
fkazu <ketuago.kazu@outlook.jp>
2021-04-12 15:39:17 +0000

    heroHeaderMorphingAnimeモジュールを使用して、ヒーローヘッダーのモーフィングアニメーションを実装。#1

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,9 @@ import 'picturefill';
 import 'src/sass/index.scss';
 
 import './config';
+import heroHeaderMorphingAnime from './modules/heroHeaderMorphingAnime';
+
+
+window.addEventListener( 'load', () => {
+  heroHeaderMorphingAnime.play();
+}, { once: true, passive: true } );
