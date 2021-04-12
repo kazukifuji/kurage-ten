f75328a3eec6c7d3f8007b6b30464e54aaf324f7
fkazu <ketuago.kazu@outlook.jp>
2021-04-12 16:14:14 +0000

    読み込み時にJavaScriptで、ヒーローヘッダーとシェイプマスクSVG画像をフルスクリーンサイズにリサイズさせる。#1

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,13 @@ import 'src/sass/index.scss';
 
 import './config';
 import heroHeaderMorphingAnime from './modules/heroHeaderMorphingAnime';
+import resizeToFullScreen from './modules/resizeToFullScreen';
 
+
+window.addEventListener( 'DOMContentLoaded', () => {
+  resizeToFullScreen('#hero-header');
+  resizeToFullScreen('#shape-mask > svg');
+}, { once: true, passive: true } );
 
 window.addEventListener( 'load', () => {
   heroHeaderMorphingAnime.play();
