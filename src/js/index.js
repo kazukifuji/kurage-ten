c8cd0ce9cdf6661eac0a35ae50365304339c9561
fkazu <ketuago.kazu@outlook.jp>
2021-04-15 08:06:04 +0000

    読み込み完了時にローディング画面を非表示にする。#14

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ import heroHeaderMorphingAnime from './modules/heroHeaderMorphingAnime';
 import resizeToFullScreen from './modules/resizeToFullScreen';
 import slider from './modules/slider';
 import smoothScroller from './modules/smoothScroller';
+import loadingScreenController from './modules/loadingScreenController';
 
 
 window.addEventListener( 'DOMContentLoaded', () => {
@@ -20,4 +21,11 @@ window.addEventListener( 'load', () => {
   heroHeaderMorphingAnime.play();
   slider.create('#jellyfish-list .swiper-container');
   smoothScroller('#footer .footer__move-to-top-button', 0);
+
+  loadingScreenController.close('#loading-screen', 500, 500)
+  .then(loadingScreen => {
+    loadingScreen.querySelectorAll('.loading-screen__loading-chara')
+    .forEach(loadingChara => loadingChara.style.animation = 'none');
+    loadingScreen.style.display = 'none';
+  });
 }, { once: true, passive: true } );
